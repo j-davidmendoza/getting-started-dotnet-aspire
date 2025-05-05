71415e12a75711d129600a613431ac1d2742fa4b
David Mendoza <mendoza.d.jesus@gmail.com>
2025-05-04 19:29:53 -0700

    Traces and spans

--- Module 07 - OpenTelemetry/Section 04 - Metrics/Api/Program.cs
+++ Module 07 - OpenTelemetry/Section 04 - Metrics/Api/Program.cs
@@ -15,13 +15,22 @@ builder.Services.AddHttpClient<RatingServiceHttpClient>(x => x.BaseAddress = new
 
 builder.Services.AddSingleton(TracerProvider.Default.GetTracer(builder.Environment.ApplicationName));
 
-builder.AddSqlServerDbContext<PodcastDbContext>(connectionName: "podcasts");
+builder.AddSqlServerDbContext<PodcastDbContext>(connectionName: "podcasts",
+    x => x.DisableTracing = true);
+
+builder.Services.AddOpenTelemetry().WithTracing(TracerProviderBuilder =>
+    {
+        TracerProviderBuilder.AddSqlClientInstrumentation(x => x.SetDbStatementForText = true);
+    });
 
 var app = builder.Build();
 
 app.UseCors(x => x.AllowAnyOrigin());
 
-app.MapGet("/podcasts", async (PodcastDbContext db, RatingServiceHttpClient ratingServiceHttpClient) =>
+app.MapGet("/podcasts", async (
+    PodcastDbContext db, 
+    RatingServiceHttpClient ratingServiceHttpClient,
+    Tracer tracer) =>
 {
     var all = await db.Podcasts
         .OrderBy(x => x.Title)
@@ -29,13 +38,51 @@ app.MapGet("/podcasts", async (PodcastDbContext db, RatingServiceHttpClient rati
 
     var withRatings = new List<PodcastApiModel>();
 
-    foreach (var podcast in all)
+
+
+    using (var span = tracer.StartActiveSpan("Get ratings from rating service"))
     {
-        withRatings.Add(new PodcastApiModel(
-            podcast.Title,
-            await ratingServiceHttpClient.GetRating(podcast.Title)
-        ));
+        foreach (var podcast in all)
+        {
+            withRatings.Add(new PodcastApiModel(
+                podcast.Title,
+                await ratingServiceHttpClient.GetRating(podcast.Title)
+            ));
+        }
+
+        //span.SetStatus(Status.Error);
+        //span.RecordException();
+        //trace going across messaging for pub/sub, not only https grpc
+        //be careful with spans that would sit days long
+        //thats where linkage comes in
+        //most messaging your firing a message and something picking it up straight away
+        //seeing that in a trace
+        //see the front end, calling an api, which then fires a message, then a worker picks it up and that worker maybe calls a database
+        //seeing all this in one trace is powerful
+
+        //Easy to add information to span
+        span.SetAttribute("Count", all.Count);
     }
+
+    //using (tracer.StartActiveSpan("Get ratings from rating service"))
+    //{
+    //    foreach (var podcast in all)
+    //    {
+    //        withRatings.Add(new PodcastApiModel(
+    //            podcast.Title,
+    //            await ratingServiceHttpClient.GetRating(podcast.Title)
+    //        ));
+    //    }
+    //}
+
+    //using var span = tracer.StartActiveSpan("Get ratings from rating service");
+    //foreach (var podcast in all)
+    //{
+    //    withRatings.Add(new PodcastApiModel(
+    //        podcast.Title,
+    //        await ratingServiceHttpClient.GetRating(podcast.Title)
+    //    ));
+    //}
 
     return withRatings;
 });
--- Module 07 - OpenTelemetry/Section 04 - Metrics/RatingService/Program.cs
+++ Module 07 - OpenTelemetry/Section 04 - Metrics/RatingService/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using OpenTelemetry.Trace;
 using RatingService;
+using System.Runtime.CompilerServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +25,10 @@ app.MapGet("/ratings", async ([FromQuery] string podcastName, RatingStore rating
 
     var rating = await ratingStore.GetAverageRating(podcastName);
 
+    //This is a static property called curreny span
+    Tracer.CurrentSpan.SetAttribute("PodcastName", podcastName);
+    Tracer.CurrentSpan.SetAttribute("Rating", rating);
+
     return Results.Ok(rating);
 });
 
@@ -32,7 +37,15 @@ app.MapPost("/ratings", ([FromBody] SubmitRatingRequestBody body, RatingStore ra
     if (string.IsNullOrWhiteSpace(body.PodcastName))
         return Results.BadRequest("Please provide a podcastName on the query string.");
 
+    //if in a span you have info, but you dont care when when it happens use an attirbute
+    //a span has a lifetime, within that span if you want to know when it happens within that span, use an event
+    //Can add attributes to a span at any time
+    Tracer.CurrentSpan.AddEvent("Something has happened.");
+
     ratingStore.AddRating(body.PodcastName, body.Rating);
+
+    Tracer.CurrentSpan.SetAttribute("PodcastName", body.PodcastName);
+    Tracer.CurrentSpan.SetAttribute("Rating", body.Rating);
 
     return Results.Ok();
 });
